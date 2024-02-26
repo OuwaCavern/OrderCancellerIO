646a648f20a2033129caaeefa354ac20dc49e857
Raxzero <emircan.acikgoz@hotmail.com>
2024-02-26 10:32:43 +0300

    v0.1

--- Program.cs
+++ Program.cs
@@ -8,15 +8,19 @@ namespace OrderCancellerIO
         static void Main(string[] args)
         {
             SqlConnection sqlConnection;
-            string connectionString = "Data Source=DESKTOP-NEF9RON\\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True";
+            string computerName = Environment.MachineName;
+            string? siparisNOInput;
+            string connectionString = $"Data Source={computerName}\\DESENERP;Initial Catalog=DesenPOS;Persist Security Info=True;User ID=sa;Password=DesenErp.12345;";
             try
             {
                 sqlConnection = new SqlConnection(connectionString);
                 sqlConnection.Open();
-                Console.WriteLine("Connection established.");
-                string insertQuery = "INSERT INTO production.information(name,phone,email) VALUES( 'hasan', '+9054687265', 'hasan@gmail.com')";
-                SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection);
+                Console.WriteLine("Bağlantı sağlandı.\nLütfen Sipariş NO'yu giriniz.");
+                siparisNOInput = Console.ReadLine();
+                string updateQuery = $"declare @SiparisNoYaziniz nvarchar(100)='{siparisNOInput}'\r\nupdate POSSiparis set SiparisDurumu= 8,Odendi = 0 , Kapandi = 1, SysAktif=0 where SiparisNo=@SiparisNoYaziniz;\r\n\r\ninsert into SistemTarihce\r\n(KayitId\r\n,Tablo\r\n,Tarih\r\n,RowVersion\r\n,SysAktif\r\n,SysKayitTarihi\r\n,SysKaydedenKullanici\r\n,Aciklama)\r\n\r\nselect \r\ns.Id,'POSSiparis',GETDATE(),0,1,GETDATE(),'ManuelKapatildi','güncelleme' from POSSiparis s where s.SiparisNo=@SiparisNoYaziniz";
+                SqlCommand insertCommand = new SqlCommand(updateQuery, sqlConnection);
                 insertCommand.ExecuteNonQuery();
+                sqlConnection.Close();
                 Console.WriteLine("Process is completed.");
                 Console.ReadLine();
             }
